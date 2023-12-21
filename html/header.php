9413e86f20805d4d6164241e49dd161adcddf841
FrogFromHeII <150474652+FrogFromHeII@users.noreply.github.com>
2023-12-21 16:17:06 +0100

    Add files via upload

---
+++ html/header.php
@@ -0,0 +1,38 @@
+<head>
+    <meta charset="utf-8">
+    <title>Moje stránka s recepty</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<header>
+    <div class = 'header'>
+        <h1><a class = "header-link" href="index.php">Moje stránka s recepty</a></h1>
+
+        <!-- Funkce pro systém vyhledávání -->
+        <script>
+            function showResult(str) {
+            if (str.length==0) {
+                document.getElementById("livesearch").innerHTML="";
+                document.getElementById("livesearch").style.border="0px";
+                return;
+            }
+            var xmlhttp=new XMLHttpRequest();
+            xmlhttp.onreadystatechange=function() {
+                if (this.readyState==4 && this.status==200) {
+                document.getElementById("livesearch").innerHTML=this.responseText;
+                document.getElementById("livesearch").style.border="1px solid #A5ACB2";
+                }
+            }
+            xmlhttp.open("GET","liveSearch.php?q="+str,true);
+            xmlhttp.send();
+            }
+        </script>
+        <!-- Vyhledávací pole -->
+        <form action="">
+                <label for="nz">Hledat:</label>
+                <input type="text" size="30" onkeyup="showResult(this.value)" onclick="showResult(this.value)">
+                <div id="livesearch"></div>
+            
+        </form>
+    </div>
+    </header>

418458e54633e4397080191b8bc4724b9f93cd87
FrogFromHeII <150474652+FrogFromHeII@users.noreply.github.com>
2023-12-24 23:10:02 +0100

    Update dnWeb.php

--- dnWeb.php
+++ dnWeb.php
@@ -7,7 +7,7 @@ class dnWeb {
         $this->connectToDatabase();
     }
 // připojení k databázi
-    function connectToDatabase() {
+    private function connectToDatabase() {
         include 'database.php';
         $db = new Database();
         $this->conn = $db->dbConnection();
@@ -23,17 +23,19 @@ class dnWeb {
     function insertDataToDatabase($data) {
         if ($data == true) {
             foreach ($data as $recept) {
-                $nazev = $recept['nazev'];
-                $nazev_html = $recept['nazev_html'];
-                $ingredience = $recept['ingredience'];
-                $postup = $recept['postup'];
-                $obrazek = $recept['obrazek'];
-                $cas = $recept['cas'];
-                $kategorie = $recept['kategorie'];
-
-                $sql = "INSERT INTO recepty (nazev, nazev_html, ingredience, postup, obrazek, cas, kategorie) VALUES (?, ?, ?, ?, ?, ?, ?)";
-                $stmt= $this->conn->prepare($sql);
-                $stmt->execute([$nazev, $nazev_html, $ingredience, $postup, $obrazek, $cas, $kategorie]);
+                $sql = 
+                "INSERT INTO recepty (nazev, nazev_html, ingredience, postup, obrazek, cas, kategorie) 
+                VALUES (:nazev, :nazev_html, :ingredience, :postup, :obrazek, :cas, :kategorie)";
+                $params = [
+                    'nazev' => $recept['nazev'], 
+                    'nazev_html' => $recept['nazev_html'], 
+                    'ingredience' => $recept['ingredience'], 
+                    'postup' => $recept['postup'], 
+                    'obrazek' => $recept['obrazek'], 
+                    'cas' => $recept['cas'], 
+                    'kategorie' => $recept['kategorie']
+                ];
+                $this->executeQuery($sql, $params);
             }
         }
     }
@@ -44,62 +46,42 @@ class dnWeb {
     }
 
 // načtení a zobrazení dat z databáze, podle volitelného SQL a parametrů
-function displayDataFromDatabase($sql, $params = array()) {
-    $stmt = $this->conn->prepare($sql);
-    $stmt->execute($params);
-    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    function displayDataFromDatabase($sql, $params = array()) {
+        $result = $this->executeQuery($sql, $params, true);
 
-    if (count($result) > 0) {
-        foreach ($result as $row) {
-            echo "<a class = 'rowFrontPage' href='recept.php?id=" . $row["id"] ."-nz=". $row["nazev"] . "'>";
-            echo "<img src ='" . $row['obrazek'] . "'class = 'frontPageItemPicture' alt = 'Obrázek k receptu'>"
-            . "<div class = frontPageText>" . $row["nazev"] . "</div>";
-            echo "</a>";
+        if (count($result) > 0) {
+            foreach ($result as $row) {
+                echo "<a class = 'rowFrontPage' href='recept.php?id=" . $row["id"] ."-nz=". $row["nazev"] . "'>";
+                echo "<img src ='" . $row['obrazek'] . "'class = 'frontPageItemPicture' alt = 'Obrázek k receptu'>"
+                . "<div class = frontPageText>" . $row["nazev"] . "</div>";
+                echo "</a>";
+            }
+        } else {
+            echo "V této kategorii nejsou žádné recepty.";
         }
-    } else {
-        echo "V této kategorii nejsou žádné recepty.";
     }
-}
 
-// volitelné načtení dat z volitelné databáze
+// obecné načtení dat z volitelné databáze
     function getDataFromDatabase($columns, $table) {
         $sql = "SELECT " . implode(", ", $columns) . " FROM " . $table;
-        $result = $this->conn->query($sql);
-        return $result;
+        return $this->executeQuery($sql, [], true);
     }
 
-// získání receptu podle ID
-    function getReceptById($id) {
-        $sql = "SELECT recepty.*, kategorie.nazev AS kategorie_nazev FROM recepty 
-                JOIN kategorie ON recepty.kategorie = kategorie.id 
-                WHERE recepty.id = :id";
+// obecná funkce s povinými argumenty pro SQL příkaz a pro parametry k němu
+    function executeQuery($sql, $params, $fetchAll = false) {
         $stmt = $this->conn->prepare($sql);
-        $stmt->execute(['id' => $id]);
-        return $stmt->fetch(PDO::FETCH_ASSOC);
+        $stmt->execute($params);
+        if ($fetchAll) {
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } else {
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        }
     }
-
-// získání průměrného hodnocení receptu
-    function getAverageRating($id) {
-        $sql = "SELECT AVG(hodnoceni) as average_rating FROM hodnoceni WHERE recept = :id";
-        $stmt = $this->conn->prepare($sql);
-        $stmt->execute(['id' => $id]);
-        return $stmt->fetch(PDO::FETCH_ASSOC);
-    }
-
-// získání celkového počtu hodnocení
-    function getTotalRatings($id) {
-        $sql = "SELECT COUNT(*) as total FROM hodnoceni WHERE recept = :id";
-        $stmt = $this->conn->prepare($sql);
-        $stmt->execute(['id' => $id]);
-        $result = $stmt->fetch(PDO::FETCH_ASSOC);
-        return $result['total'];
-    }
-
+    
 // přidání hodnocení receptu
     function addRating($recept, $hodnoceni) {
         $sql = "INSERT INTO hodnoceni (recept, hodnoceni) VALUES (:recept, :hodnoceni)";
-        $stmt = $this->conn->prepare($sql);
-        $stmt->execute(['recept' => $recept, 'hodnoceni' => $hodnoceni]);
+        $this->executeQuery($sql, ['recept' => $recept, 'hodnoceni' => $hodnoceni]);
     }
 
 // Rozbalovací okno se všemi kategoriemi z databáze
@@ -107,18 +89,16 @@ function displayDataFromDatabase($sql, $params = array()) {
         // Získání kategorií z databáze
         $columns = array("id", "nazev");
         $result = $this->getDataFromDatabase($columns, "kategorie");
-
         // Vytvoření rozbalovacího seznamu
         $dropdown = "<select onchange='window.location.href=this.value;'>\n";
         $dropdown .= "<option value=''>Vyberte kategorii</option>\n";
-        while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        foreach($result as $row) {
             $dropdown .= "<option value='category.php?id=" . $row["id"] . "'>" . $row["nazev"] . "</option>\n";
         }
         $dropdown .= "</select>\n";
-
         return $dropdown;
     }
-    
+  
 // odpojení databáze
     function closeConnection() {
         $this->conn = null;
